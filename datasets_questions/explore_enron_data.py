83f3060daad0f3ce8e04aa2a6f246e6996b27f00
David Speck <david@saduras.de>
2018-06-02 13:51:52 +0200

    Print more example info from enron data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -21,10 +21,15 @@ enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r")
 
 
 print "persons:",len(enron_data)
-print "features:",len(enron_data.values()[0])
+print "feature count:",len(enron_data.values()[0])
+print "features",enron_data.values()[0].keys()
 
 poi_count = 0
 for key in enron_data.keys():
     poi_count = poi_count + enron_data[key]['poi']
 
 print "person of interests", poi_count
+
+print "James Prentice total stock value:",enron_data["PRENTICE JAMES"]["total_stock_value"]
+print "Wesley Colwell message to POIs:", enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print "Jeffrey K Skilling excercied stock value:",enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
