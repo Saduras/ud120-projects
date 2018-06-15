ef9c85a9230d3d15051b903c3885b0c40295c3b8
David Speck <david@saduras.de>
2018-06-15 13:58:18 +0200

    TextPre: Stem words of message

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,15 +28,20 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        split = text_string.split(" ")
+        words = ""
+        from nltk.stem.snowball import SnowballStemmer
+        stemmer = SnowballStemmer("english")
 
-
-
+        for i in range(len(split)):
+            stemmed = stemmer.stem(split[i])
+            if len(stemmed) > 0:
+                words = words + stemmed + " "
 
     return words
 
